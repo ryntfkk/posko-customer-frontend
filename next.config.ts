b3732a4a56b2ecc6a4063faf4c2a84fddedb5cc3
posko24 apps <ryntfk@gmail.com>
2025-11-25 09:33:22 +0700

    new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     // [FIX] Izinkan SVG dari domain eksternal (Dicebear)
     dangerouslyAllowSVG: true,
     // Opsional: Tambahkan header keamanan untuk SVG
