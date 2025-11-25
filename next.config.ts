068e0710beaa44bf5424b347624ea22ee4df96c4
posko24 apps <ryntfk@gmail.com>
2025-11-25 11:27:35 +0700

    new

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
